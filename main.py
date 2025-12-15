df874628a696c95c19d0f880fcc56378b3e93049
R. Kanauchi <s258605s@st.go.tuat.ac.jp>
2025-12-15 23:16:39 +0900

    hint

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import lightning as L
 
+from typing import Union,Any
+
 # define any number of nn.Modules (or use your current ones)
 encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
 decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
@@ -11,12 +13,15 @@ decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
 
 # define the LightningModule
 class LitAutoEncoder(L.LightningModule):
-    def __init__(self, encoder, decoder):
+    def __init__(self: L.LightningModule, encoder: nn.Sequential, decoder: nn.Sequential):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self: L.LightningModule, 
+                      batch: Union[list, tuple, range, set, frozenset], 
+                      batch_idx: Any
+    ) -> nn.functional:
         # training_step defines the train loop.
         # it is independent of forward
         x, _ = batch
@@ -28,7 +33,7 @@ class LitAutoEncoder(L.LightningModule):
         self.log("train_loss", loss)
         return loss
 
-    def configure_optimizers(self):
+    def configure_optimizers(self: L.LightningModule) -> optim.Adam:
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
